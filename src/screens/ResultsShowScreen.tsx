e378d6ca1d3f8461b78dc767f2acb68d44d5a801
aliff azfar <aliffazfar34@gmail.com>
2022-04-05 11:59:32 +0800

    fetching single restaurant data for preview

--- src/screens/ResultsShowScreen.tsx
+++ src/screens/ResultsShowScreen.tsx
@@ -1,15 +1,27 @@
-import React, { FC } from 'react'
+import React, { FC, useState, useEffect } from 'react'
 import { Text, View, StyleSheet } from 'react-native'
+import yelp from '../api/yelp'
 
 interface ResultProp {
   navigation: any
 }
 
 const ResultsShowScreen: FC<ResultProp> = ({ navigation }) => {
+  const [result, setResult] = useState<any>(null)
+
   const id = navigation.getParam('id')
+
+  const getResult = async (id: any) => {
+    const response = await yelp.get(`/${id}`)
+    setResult(response.data)
+  }
+  useEffect(() => {
+    getResult(id)
+  }, [])
+
   return (
     <View>
-      <Text>{id}</Text>
+      <Text>Rsults shows</Text>
     </View>
   )
 }
